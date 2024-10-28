00ef5f201d57659e4e3b93a8be030f9549192c61
Oleg Borodatov <oleg.borodatov@emergingtravel.com>
2024-10-28 19:06:02 +0700

    feat(auth): add user events

--- server/Users.cs
+++ server/Users.cs
@@ -1,3 +1,4 @@
+using System.Threading.Channels;
 using App.Db;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Options;
@@ -8,11 +9,20 @@ public class User : IdentityUser {
 
 }
 
-public class ApplicationUserManager(IUserStore<User> store, IOptions<IdentityOptions> optionsAccessor, IPasswordHasher<User> passwordHasher, IEnumerable<IUserValidator<User>> userValidators, IEnumerable<IPasswordValidator<User>> passwordValidators, ILookupNormalizer keyNormalizer, IdentityErrorDescriber errors, IServiceProvider services, ILogger<UserManager<User>> logger) : UserManager<User>(store, optionsAccessor, passwordHasher, userValidators, passwordValidators, keyNormalizer, errors, services, logger) {
+public abstract record UserEvent;
+public record UserCreated(User user) : UserEvent;
+
+public class ApplicationUserManager : UserManager<User> {
+  private readonly Channel<UserEvent> _channel;
+  public ApplicationUserManager(IUserStore<User> store, IOptions<IdentityOptions> optionsAccessor, IPasswordHasher<User> passwordHasher, IEnumerable<IUserValidator<User>> userValidators, IEnumerable<IPasswordValidator<User>> passwordValidators, ILookupNormalizer keyNormalizer, IdentityErrorDescriber errors, IServiceProvider services, ILogger<UserManager<User>> logger, Channel<UserEvent> channel) : base(store, optionsAccessor, passwordHasher, userValidators, passwordValidators, keyNormalizer, errors, services, logger) {
+    _channel = channel;
+  }
+
   public override async Task<IdentityResult> CreateAsync(User user, string password) {
     var result = await base.CreateAsync(user, password);
 
     if (result.Succeeded) {
+      await _channel.Writer.WriteAsync(new UserCreated(user));
       Logger.LogInformation("User Created");
     }
 
@@ -22,6 +32,7 @@ public class ApplicationUserManager(IUserStore<User> store, IOptions<IdentityOpt
 
 public static class AuthExtensions {
   public static void AddAuthServices(this IServiceCollection services, IWebHostEnvironment environment) {
+    services.AddSingleton(Channel.CreateUnbounded<UserEvent>());
     // https://learn.microsoft.com/en-us/aspnet/core/security/authentication/identity-configuration?view=aspnetcore-8.0
     // https://learn.microsoft.com/en-us/aspnet/core/security/authentication/identity?view=aspnetcore-8.0&tabs=net-cli#examine-register
     // https://learn.microsoft.com/en-us/aspnet/core/security/authentication/customize-identity-model?view=aspnetcore-8.0#change-the-primary-key-type
